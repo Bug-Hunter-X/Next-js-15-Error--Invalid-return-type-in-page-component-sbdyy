688951c744be1abfb98b7277b0496c252600b22e
BugHunter-X <askzenai@gmail.com>
2024-12-23 19:58:11 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,24 @@
+```javascript
+// pages/index.js
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app!</h1>
+      <Link href="/about">
+        <a>Go to About Page</a>
+      </Link>
+    </div>
+  );
+}
+```
+```javascript
+// pages/about.js
+
+export default function About() {
+  // This will cause an error in Next.js 15
+  // because it's not a valid React component
+  return <p>This is the about page!</p>;
+}
+```
